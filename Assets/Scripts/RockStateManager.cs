a2f0475e67e3690887fb799e27718c73b88c03c0
aplaxco1 <aplaxco@ucsc.edu>
2024-05-23 12:56:32 -0700

    puttin some stat variables in there

--- Assets/Scripts/RockStateManager.cs
+++ Assets/Scripts/RockStateManager.cs
@@ -7,6 +7,11 @@ public class RockStateManager : MonoBehaviour
 
     RockState currentState;
 
+    static public float attention = 50f;
+    static public float hunger = 50f;
+    static public float thirst = 50f;
+    static public float moss = 50f;
+
     // Start is called before the first frame update
     void Start()
     {
